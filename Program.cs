73f1fab324000e4e94df3fa8e6f84e150ee6f7bf
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 15:12:16 -0700

    Use Book class with parameters in constructor

--- Program.cs
+++ Program.cs
@@ -10,19 +10,13 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
 
             Console.WriteLine(book1.title);
             Console.WriteLine(book1.author);
             Console.WriteLine(book1.pages);
 
-            Book book2 = new Book();
-            book2.title = "Lord of the Rings";
-            book2.author = "Tolkein";
-            book2.pages = 700;
+            Book book2 = new Book("Lord of the Rings", "Tolkein", 700);
 
             Console.WriteLine(book2.title);
             Console.WriteLine(book2.author);
