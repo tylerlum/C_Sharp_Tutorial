efa9c49cc4003fe96cc698153cca00bf03409185
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 14:59:43 -0700

    Use try catch block with print of exception message

--- Program.cs
+++ Program.cs
@@ -10,24 +10,22 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
-
-            for (int i = 0; i < luckyNumbers.Length; i++)
+            try
             {
-                Console.WriteLine(GetPow(luckyNumbers[i], 3));
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
             }
 
             Console.ReadLine();
         }
 
-        static int GetPow(int baseNum, int powNum)
-        {
-            int retVal = 1;
-            for (int i = 0; i < powNum; i++)
-            {
-                retVal *= baseNum;
-            }
-            return retVal;
-        }
     }
 }
