0ec5e0df3b4c485bf5819f1dd0fe5f3ebc409cf9
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 14:22:12 -0700

    Use array

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            string characterName = "John";
-            int characterAge = 35;
-            Console.WriteLine("There once was a man named " + characterName);
-            Console.WriteLine("He was " + characterAge + " years old");
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+
+            Console.WriteLine(friends[1]);
 
             Console.ReadLine();
         }
