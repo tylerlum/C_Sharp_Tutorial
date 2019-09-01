da9932bfacbfe9e38623ab40266ce587735faca9
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 14:23:24 -0700

    Use function

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
-            string[] friends = new string[5];
-            friends[0] = "Jim";
-            friends[1] = "Kelly";
-
-            Console.WriteLine(friends[1]);
+            SayHi();
 
             Console.ReadLine();
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hello user");
         }
     }
 }
