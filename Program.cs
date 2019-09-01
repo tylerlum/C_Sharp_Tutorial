9de14e38e0cbf04239e93ed7891bd45fabbbe422
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 14:20:41 -0700

    Add simple variable use and print

--- Program.cs
+++ Program.cs
@@ -10,7 +10,11 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string characterName = "John";
+            int characterAge = 35;
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
             Console.ReadLine();
         }
     }
