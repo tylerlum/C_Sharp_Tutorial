a295243a97fc71dd311970198e6a716483625b48
Tyler Lum <tylergwlum@gmail.com>
2019-09-01 14:32:47 -0700

    Use switch case statements

--- Program.cs
+++ Program.cs
@@ -10,42 +10,42 @@ namespace Giraffe
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Enter an operator: ");
-            string op = Console.ReadLine();
-
-            Console.Write("Enter a number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            if (op == "+")
-            {
-                Console.WriteLine(num1 + num2);
-            }
-            else if (op == "-")
-            {
-                Console.WriteLine(num1 - num2);
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine(num1 / num2);
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine(num1 * num2);
-            }
-            else
-            {
-                Console.WriteLine("Invalid operator");
-            }
-
+            Console.WriteLine(GetDay(5));
             Console.ReadLine();
         }
 
-        static void SayHi(string name)
+        static string GetDay(int dayNum)
         {
-            Console.WriteLine("Hello user " + name);
+            string dayName;
+
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName;
         }
     }
 }
